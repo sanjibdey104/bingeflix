36fccc5c99e1e66771da9d8513aa9be3d062aefd
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-09-26 21:46:32 +0530

    refactor: code cleanup

--- src/components/GenreOptions/GenreOptions.style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledGenreOptions = styled.section`
-  position: absolute;
-  top: 7.5rem;
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-`;
